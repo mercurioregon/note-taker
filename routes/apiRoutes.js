10f1461b3d873855b4536ebde6470cc58f0a7ca3
mercurioregon <jamesm620@gmail.com>
2024-05-11 13:42:54 -0700

    delete issue solved

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,6 +7,7 @@ const path = require("path")
 router.get("/notes", (req,res)=> {
     fs.readFile(path.join(__dirname,"../db/db.json" ),"utf8",(err, data) =>{
         if (err){console.log ("err")}
+   
         res.json(JSON.parse(data))
     } )
 })
@@ -27,5 +28,17 @@ router.post("/notes", (req,res) => {
     } )
 })
 
-
+router.delete("/notes/:id", (req,res) => {
+   const noteId = parseInt(req.params.id)
+    fs.readFile(path.join(__dirname,"../db/db.json" ),"utf8",(err, data) =>{
+        if (err){console.log ("err")}
+        const notes = JSON.parse(data)
+        const filterNotes = notes.filter(note => note.id !== noteId) 
+        console.log(filterNotes)
+       fs.writeFile(path.join(__dirname,"../db/db.json"  ), JSON.stringify(filterNotes), (err)=> {
+        if (err){console.log ("err")}
+        res.send(filterNotes)
+       } )
+    } )
+})
 module.exports = router
