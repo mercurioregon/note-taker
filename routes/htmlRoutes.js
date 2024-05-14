cd103ade47199d72e95d69facb52cb89a21f7190
mercurioregon <jamesm620@gmail.com>
2024-05-13 18:14:52 -0700

    something

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,11 +5,11 @@ const fs = require ("fs")
 
 const path = require("path")
 router.get("/notes", (req,res)=> {
-    res.sendFile(path.join(__dirname, "..","public","notes.html"))
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
 },
 )
 router.get("/", (req,res)=> {
-    res.sendFile(path.join(__dirname,".." ,"public","index.html" ))
+    res.sendFile(path.join(__dirname,"../public/index.html" ))
 })
 
 module.exports = router
